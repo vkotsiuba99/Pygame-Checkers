b93dcdf6f85ad1ab2c5b03b70a5ad07d62e0b084
vkotsiuba99 <92172262+vkotsiuba99@users.noreply.github.com>
2022-02-11 18:14:53 +0200

    Added pieces

--- board.py
+++ board.py
@@ -16,3 +16,5 @@ class Board:
             for col in range(row % 2, ROWS, 2):
                 pygame.draw.rect(win, RED, (row * SQUARE_SIZE, col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
+    def create_board(self):
+        pass
